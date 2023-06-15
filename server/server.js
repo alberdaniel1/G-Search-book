61eb1522c767abd755b4d1f0b6ed91c45c1c08f8
Daniel Avelar <danielavelar@Daniels-MacBook-Pro.local>
2023-06-15 09:44:05 -0400

    something

--- server/server.js
+++ server/server.js
@@ -10,12 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+// add below two lines for deployment
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
